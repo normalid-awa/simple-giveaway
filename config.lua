c8cf3c7d61fe4d94cbc4b59df434d6deadc35144
jkh675 <52519933+jkh675@users.noreply.github.com>
2021-05-03 09:02:32 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {}
-Config.Locale = 'zh'
-Config.code = 'CVX-412'
-Config.money = 1000 -- text to show
-Config.Maxuse = 10
+Config.Locale = 'zh' --language
+Config.code = 'CVX-412' --code
+Config.money = 1000 --givemoney
+Config.Maxuse = 10 --max useage
 --倒卖死全家 
 --resell die mom
